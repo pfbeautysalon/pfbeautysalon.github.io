33580add65b142d4142bdc4e4731cfebf3f73138
PrincetonPoh <PrincetonPoh@users.noreply.github.com>
2025-03-24 15:24:01 +0000

    deploy: 69f3bc84e74312e9693a6b232125d98bcc0ad0ef

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 module.exports = {
-    content: [
-        './_drafts/**/*.html',
-        './_includes/**/*.html',
-        './_layouts/**/*.html',
-        './_posts/*.md',
-        './*.md',
-        './*.html',
-    ],
-    theme: {
-        extend: {
-            colors:
-            {
-                'pp-background': '#F3F3F5',
-                'pp-background-light': '#CCCCD1',
-                'pp-yellow': '#FCB544',
-                'pp-yellow-light': '#FCCF88',
-                'pp-blue': '#3C4BE1',
-                'pp-blue-light': '#7982DE',
-                'pp-green': '#90CF65',
-                'pp-green-light': '#BEF29A',
-                'pp-red': '#FF5645',
-            },
-        },
+  content: [
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', "serif"],
+        sans: ['"Playfair Display"', "serif"],
+      },
+      colors: {
+        "pp-background": "#F3F3F5",
+        "pp-background-light": "#CCCCD1",
+        "pp-yellow": "#FCB544",
+        "pp-yellow-light": "#FCCF88",
+        "pp-blue": "#3C4BE1",
+        "pp-blue-light": "#7982DE",
+        "pp-green": "#90CF65",
+        "pp-green-light": "#BEF29A",
+        "pp-red": "#FF5645",
+      },
     },
-    plugins: []
-}
+  },
+  plugins: [],
+};
